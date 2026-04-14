b65d8b242b5763ac0094ce1f975c676ec470158f
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-14 15:52:01 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+# groupAnagrams
+# Hash table
+# TC: O(m*nlogn)
+# SC: O(m*n)
+# Logic: Hash table represent every string by frequency
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
