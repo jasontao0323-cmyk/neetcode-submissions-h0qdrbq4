bec5a6c0bc6e5f5897b0f0f3da56400624700e5c
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-16 14:00:27 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,11 @@
+# MaxArea
+# TC: O(n^2)
+# SC: O(1)
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        res = 0
+        for i in range(len(heights)):
+            for j in range(i + 1, len(heights)):
+                res = max(res, min(heights[i], heights[j]) * (j - i))
+        return res
