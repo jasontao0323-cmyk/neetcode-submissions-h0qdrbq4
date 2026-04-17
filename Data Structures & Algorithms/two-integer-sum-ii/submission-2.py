773b2d70696428f502aa3e6932838770f0d86140
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-17 15:58:48 -0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,20 @@
+# Two Sum
+# Two Pointers, the array is sorted, we can use two pointers to adjust the sum effiently
+# TC: O(n)
+# SC: O(1)
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
