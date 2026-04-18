9b78ab32611885da0c10d920905864c84abcac4d
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-18 11:41:31 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,22 @@
+# isValid
+# Brute Force: If the string is valid, we can repeately 
+# remove these matching pairs until nothing is left
+# O(n)
+# O(n)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+        
