44adea4965dd29c8adcf6ef07ee428df6236fc8e
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-22 16:18:19 -0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,17 @@
+# characterReplacement 
+# Brute-force approach checks all substrings, expanding each and tracking character frequencies to 
+# determine validity based on replacement limits.
+# TC: O(n^2)
+# SC: O(m)
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        for i in range(len(s)):
+            count, maxf = {}, 0
+            for j in range(i, len(s)):
+                count[s[j]] = 1 + count.get(s[j], 0)
+                maxf = max(maxf, count[s[j]])
+                if (j - i + 1) - maxf <= k:
+                    res = max(res, j - i + 1)
+        return res
