0796f64309b7181e4d7d173aa3f8ad5cbbeacd4b
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-23 11:14:04 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,24 @@
+# checkInclusion
+# Hash Table Character frequencies in s1 are counted to identify valid substrings in s2. A frequency map is built for 
+# each starting point in s2, stopping early if counts exceed those in s1.
+# TC: O(n*m)
+# SC: O(1)
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
