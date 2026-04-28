3f6c0cdb96041361f08b29404adc2a5677b7a5d8
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-28 13:15:18 -0700

    Add: binary-tree-diameter - submission-3

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-3.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Iterative DFS can simulate 
+# recursive DFS to compute diameter without using the call stack.
+# TC: O(n)
+# Sc: O(n)
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        stack = [root]
+        mp = {None: (0, 0)}
+
+        while stack:
+            node = stack[-1]
+
+            if node.left and node.left not in mp:
+                stack.append(node.left)
+            elif node.right and node.right not in mp:
+                stack.append(node.right)
+            else:
+                node = stack.pop()
+
+                leftHeight, leftDiameter = mp[node.left]
+                rightHeight, rightDiameter = mp[node.right]
+
+                mp[node] = (1 + max(leftHeight, rightHeight),
+                           max(leftHeight + rightHeight, leftDiameter, rightDiameter))
+
+        return mp[root][1]
