5dba3fb3b21a359eca99a1337a3864778149ccd2
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-04-28 13:12:40 -0700

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Breadth-First Search (BFS) is ideal for computing maximum tree depth as it processes each level 
+# sequentially, with each completed level increasing the depth by one.
+# TC: O(n)
+# SC: O(n)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
