b582bbde7710236d6ec02926fd3b108997c59714
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-01 10:06:07 -0700

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# The maximum path sum in a binary tree is calculated using a depth-first search (DFS) algorithm. The algorithm computes the maximum downward path for each node and updates the global maximum path sum accordingly. 
+# The time complexity is O(n^2) and the space complexity is O(n).
+# TC: O(n)
+# SC: O(n)
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return res[0]
