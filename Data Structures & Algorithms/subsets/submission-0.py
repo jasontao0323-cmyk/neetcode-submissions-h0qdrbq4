7051b7b2faee75ff40d14c743826a1783cf8df03
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-02 15:22:09 -0700

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,19 @@
+# Backtracking generates all 2ⁿ subsets by exploring 
+# inclusion and exclusion of each number, forming a decision tree.
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
