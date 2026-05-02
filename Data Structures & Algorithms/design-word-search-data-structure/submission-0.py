962e1f6aa01dd4f925d8dd9e9c5985fb92f2c0a1
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-02 15:15:40 -0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,26 @@
+# Store words and check 
+# each during search. Exact match required unless search character is a wildcard.
+# TC: O（1) for addWord()，O（m*n) for search()
+# SC: O(m*n)
+
+class WordDictionary:
+
+    def __init__(self):
+        self.store = []
+
+    def addWord(self, word: str) -> None:
+        self.store.append(word)
+
+    def search(self, word: str) -> bool:
+        for w in self.store:
+            if len(w) != len(word):
+                continue
+            i = 0
+            while i < len(w):
+                if w[i] == word[i] or word[i] == '.':
+                    i += 1
+                else:
+                    break
+            if i == len(w):
+                return True
+        return False
