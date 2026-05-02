b285eeaf25abbb756ecbaaa0b78908df5f23b3e7
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-02 15:21:19 -0700

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,68 @@
+# The text discusses three approaches to solving the Word Search II problem using backtracking. The first approach modifies the board to mark visited cells, the second uses a Trie to guide the search and avoid unnecessary paths, and the third combines a Trie with aggressive pruning to remove branches that no longer 
+# lead to valid words. Each approach has its own time and space complexity.
+# TC: O(m*n*4*3^t-1+s)
+# SC: O(s)
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.idx = -1
+        self.refs = 0
+
+    def addWord(self, word, i):
+        cur = self
+        cur.refs += 1
+        for c in word:
+            index = ord(c) - ord('a')
+            if not cur.children[index]:
+                cur.children[index] = TrieNode()
+            cur = cur.children[index]
+            cur.refs += 1
+        cur.idx = i
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for i in range(len(words)):
+            root.addWord(words[i], i)
+
+        ROWS, COLS = len(board), len(board[0])
+        res = []
+
+        def getIndex(c):
+            index = ord(c) - ord('a')
+            return index
+
+        def dfs(r, c, node):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or board[r][c] == '*' or
+                not node.children[getIndex(board[r][c])]):
+                return
+
+            tmp = board[r][c]
+            board[r][c] = '*'
+            prev = node
+            node = node.children[getIndex(tmp)]
+            if node.idx != -1:
+                res.append(words[node.idx])
+                node.idx = -1
+                node.refs -= 1
+                if not node.refs:
+                    prev.children[getIndex(tmp)] = None
+                    node = None
+                    board[r][c] = tmp
+                    return
+
+            dfs(r + 1, c, node)
+            dfs(r - 1, c, node)
+            dfs(r, c + 1, node)
+            dfs(r, c - 1, node)
+
+            board[r][c] = tmp
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root)
+
+        return res
+
