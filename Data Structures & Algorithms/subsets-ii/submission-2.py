743304fdb8f584c082e1ca25b13700ec206babe8
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-04 14:55:55 -0700

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.py
@@ -0,0 +1,25 @@
+# Iterative method builds subsets, avoiding duplicates by 
+# sorting the array and using two indices to control subset generation.
+# TC: O(n*2^n)
+# SC: O(1) for extra space, O(2^n) space for the outpost list
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
