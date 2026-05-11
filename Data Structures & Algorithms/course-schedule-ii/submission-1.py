652d1437902a7a5306c2bbc1f8e732848b372e42
jasontao0323-cmyk <jasontao0323@icloud.com>
2026-05-11 11:47:08 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,28 @@
+# Topological sorting with 
+# DFS traversal ensures course prerequisites are met.
+# TC: O(V+E)
+# SC: O(V+E)
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(numCourses)]
+        indegree = [0] * numCourses
+        for nxt, pre in prerequisites:
+            indegree[nxt] += 1
+            adj[pre].append(nxt)
+
+        output = []
+
+        def dfs(node):
+            output.append(node)
+            indegree[node] -= 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    dfs(nei)
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dfs(i)
+
+        return output if len(output) == numCourses else []
